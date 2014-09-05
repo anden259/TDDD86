8756f2d8c5657fd2717dc3aafddf8fb630ab8fd3
andno037 <andno037@student.liu.se>
2014-09-05 15:11:44 +0200

    first lab leasson

--- lab2/evilhangman/src/evilhangman.cpp
+++ lab2/evilhangman/src/evilhangman.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
 #include <string>
+#include <string>
+#include <queue>
+#include <vector>
+#include <map>
+#include <fstream>
 using namespace std;
 
 const string alphabet  = "abcdefghijklmnopqrstuvwxyz";
 
+template<typename T>
+void fillmap(map<int, T>& myMap){
+    string word;
+    ifstream myReader;
+    myReader.open("dictionary.txt");
+    while(myReader >> word){
+        if (myMap.find(word.length()) != myMap.end()){
+                myMap.find(word.length())->second.push_back(word);
+        }else{
+            myMap.insert(pair<int, T>(word.length(), T{word}));
+        }
+    }
+    myReader.close();
+}
+
+template<typename T>
+map<string, T> getfamily(T& word_list, char c){
+    map<string, T> ret;
+    for(auto word:word_list){
+        string line = "";
+        for(auto symbol:word){
+            if(symbol == c){
+                line += c;
+            }else{
+                line += "-";
+            }
+        }
+    }
+}
+
+
 int main() {
+    map<int, vector<string> > myMap;
+    fillmap(myMap);
+
+    cout << myMap.find(2)->second[1] <<endl;
     cout << "Welcome to Hangman." << endl;
 
     // TODO: Finish the program!
--- life/src/life.cpp
+++ life/src/life.cpp
@@ -1,47 +1,50 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
+// andno037, anden259
 
-#include <iostream>
+#include  <iostream>
 #include "grid.h"
 #include "lifeutil.h"
-#include <fstream>
+#include  <fstream>
 using namespace std;
 
-template <typename T>
-void print_grid(Grid<T>& grid){
-    for(int r=0;r<grid.numRows();r++){
-        for(int c=0;c<grid.numCols();c++){
-            cout << grid.get(r,c);
+// Prints the grid to cout
+template  <typename T>
+void print_grid(Grid <T>& grid)
+{
+    for(int r = 0; r < grid.numRows(); r++) {
+        for(int c = 0; c < grid.numCols(); c++) {
+            cout  <<  grid.get(r,c);
         }
-        cout <<endl;
+        cout  << endl;
     }
-
 }
 
-template <typename T>
-void fill_grid(Grid<T>& grid, T filler){
-    for(int r=0;r<grid.numRows();r++){
-        for(int c=0;c<grid.numCols();c++){
+// Filles the grid
+template  <typename T>
+void fill_grid(Grid <T>& grid, T filler)
+{
+    for(int r = 0; r < grid.numRows(); r++) {
+        for(int c = 0; c < grid.numCols(); c++) {
             grid.set(r,c, filler);
         }
     }
 }
 
-template <typename T>
-void evolve(Grid<T>& grid){
-    int numRows = grid.numRows();
-    int numCols = grid.numCols();
-    Grid<int> tmp_grid(numRows,numCols);
+// Evolves the grid to the next generation.
+template  <typename T>
+void evolve(Grid <T>& grid)
+{
+    int numRows  =  grid.numRows();
+    int numCols  =  grid.numCols();
+    Grid < int> tmp_grid(numRows,numCols);
     fill_grid(tmp_grid,0);
 
-    for(int r=0;r<numRows;r++){
-        for(int c=0;c<numCols;c++){
-            if(grid.get(r,c) == 'X'){
-                for(int i=-1; i<=1; i++){
-                    for(int j=-1;j<=1;j++){
-                        if(!((i==0) && (j==0)) && tmp_grid.inBounds(r+i,c+j)){
-                            tmp_grid.set(r+i,c+j,tmp_grid.get(r+i,c+j)+1);
+    for(int r = 0; r < numRows; r++) {
+        for(int c = 0; c < numCols; c++) {
+            if(grid.get(r,c)  ==  'X') {
+                for(int i = -1; i <= 1; i++) {
+                    for(int j = -1; j <= 1; j++) {
+                        if(!((i == 0) && (j == 0)) && tmp_grid.inBounds(r + i,c + j)) {
+                            tmp_grid.set(r + i,c + j,tmp_grid.get(r + i,c + j) + 1);
                         }
                     }
                 }
@@ -49,8 +52,8 @@ void evolve(Grid<T>& grid){
         }
     }
 
-    for(int r=0;r<numRows;r++){
-        for(int c=0;c<numCols;c++){
+    for(int r = 0; r < numRows; r++) {
+        for(int c = 0; c < numCols; c++) {
             switch (tmp_grid.get(r,c)) {
             case 2:
                 break;
@@ -64,57 +67,58 @@ void evolve(Grid<T>& grid){
         }
     }
 }
-template <typename T>
-void animation(Grid<T>& grid){
-    while(true){
-    print_grid(grid);
-    pause(150);
-    clearConsole();
-    evolve(grid);
+
+// Starts an auto evalve of the grid.
+template  <typename T>
+void animation(Grid <T>& grid)
+{
+    while(true) {
+        print_grid(grid);
+        pause(150);
+        clearConsole();
+        evolve(grid);
     }
 }
 
 
-int main() {
+int main()
+{
     string filename,line;
     int row,col;
     string number;
     ifstream myReader;
-    // TODO: Finish the program!
 
-    cout << "Welcome to the TDDD86 Game of Life,\n";
-    cout << "a simulation of the lifecycle of a bacteria colony.\n";
-    cout << "Cells (X) live and die by the following rules:\n";
-    cout << " - A cell with 1 or fewer neighbours dies.\n";
-    cout << " - Location with 2 neighbours remain stable.\n";
-    cout << " - Location with 3 neighbours will crerat life.\n";
-    cout << " - A cell with 4 or more neighbours dies.\n";
-    cout << "\n";
+    cout  <<  "Welcome to the TDDD86 Game of Life,\n";
+    cout  <<  "a simulation of the lifecycle of a bacteria colony.\n";
+    cout  <<  "Cells (X) live and die by the following rules:\n";
+    cout  <<  " - A cell with 1 or fewer neighbours dies.\n";
+    cout  <<  " - Location with 2 neighbours remain stable.\n";
+    cout  <<  " - Location with 3 neighbours will crerat life.\n";
+    cout  <<  " - A cell with 4 or more neighbours dies.\n";
+    cout  <<  "\n";
 
-    cout << "Grid input file name? ";
+    cout  <<  "Grid input file name? ";
     cin >> filename;
-    //filename="simple.txt";
+    //filename = "simple.txt";
     myReader.open(filename);
 
     getline(myReader,line);
-    row=atoi(&line[0]);
+    row = atoi(&line[0]);
     getline(myReader,line);
-    col=atoi(&line[0]);
+    col = atoi(&line[0]);
 
+    Grid <char> myGrid(row,col);
 
-    Grid<char> myGrid(row,col);
-
-    for(int r=0;r<row;r++){
+    for(int r = 0; r < row; r++) {
         getline(myReader,line);
-        for(int c=0;c<col;c++){
+        for(int c = 0; c < col; c++) {
             myGrid.set(r,c,line[c]);
         }
     }
 
-
     print_grid(myGrid);
     char input;
-    while(cout << "a)nimate, t)ick, q)uit? " &&cin >> input && input!='q'){
+    while(cout  <<  "a)nimate, t)ick, q)uit? " &&cin >> input && input!= 'q') {
 
         switch (input) {
         case 't':
@@ -132,10 +136,8 @@ int main() {
 
     }
 
-
-
     myReader.close();
-    cout << "Have a nice Life!" << std::endl;
+    cout  <<  "Have a nice Life!"  <<  std::endl;
     return 0;
 }
 
