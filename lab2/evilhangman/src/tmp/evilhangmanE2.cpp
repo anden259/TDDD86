6c99cede4cbd1311d57da14f26ef593ecec3c751
anden259 <anden259@users.noreply.github.com>
2014-09-29 15:05:54 +0200

    Fixed Astyle in evil. E4 done

--- lab2/evilhangman/src/tmp/evilhangmanE2.cpp
+++ lab2/evilhangman/src/tmp/evilhangmanE2.cpp
@@ -14,11 +14,12 @@ const string alphabet  = "abcdefghijklmnopqrstuvwxyz";
 
 // fillmap files the map with nummber of letters and a vector with all words with that length
 template<typename T>
-void fillmap(map<int, T>& myMap) {
+void fillmap(map<int, T>& myMap)
+{
     string word;
     ifstream myReader;
     myReader.open("dictionary.txt");
-    while(myReader >> word) {
+    while (myReader >> word) {
         if (myMap.find(word.length()) != myMap.end()) {
             myMap.find(word.length())->second.push_back(word);
         } else {
@@ -30,7 +31,8 @@ void fillmap(map<int, T>& myMap) {
 
 // get_family_string return the family string of that word
 // baste on the word start
-string get_family_string(string word, char c, string start) {
+string get_family_string(string word, char c, string start)
+{
     for (size_t i = 0; i < word.length(); i++) {
         if (start[i] == '-' && word[i] == c) {
             start[i] = c;
@@ -42,11 +44,12 @@ string get_family_string(string word, char c, string start) {
 
 // getfamily returns a map with familly string as key and a vector of string as word list
 template<typename T>
-map<string, T> getfamily(T& word_list, char c, string start) {
+map<string, T> getfamily(T& word_list, char c, string start)
+{
     map<string, T> ret;
-    for(auto word:word_list) {
+    for (auto word : word_list) {
         string family_string = get_family_string(word, c, start);
-        if(ret.find(family_string) == ret.end()) {
+        if (ret.find(family_string) == ret.end()) {
             ret.insert(ret.begin(), pair<string, T>(family_string, T {word}));
         } else {
             ret.find(family_string)->second.push_back(word);
@@ -58,13 +61,14 @@ map<string, T> getfamily(T& word_list, char c, string start) {
 // returns a pair with family string as key and a vector as element
 // the pair returnd is the one with bigest vector
 //template<typename T>
-pair<string, vector<string> > get_biggest_word_list(map<string, vector<string> >& family_map) {
+pair<string, vector<string> > get_biggest_word_list(map<string, vector<string> >& family_map)
+{
     pair <string, vector<string> > ret_map_pair;
     int max_len(0), tmp(0);
 
-    for(auto pair_in_map:family_map) {
+    for (auto pair_in_map : family_map) {
         tmp = pair_in_map.second.size();
-        if(tmp > max_len) {
+        if (tmp > max_len) {
             max_len = tmp;
             ret_map_pair = pair_in_map;
         }
@@ -73,12 +77,13 @@ pair<string, vector<string> > get_biggest_word_list(map<string, vector<string> >
 }
 
 //returns the nummber of uniqe letters in a word_list
-int number_of_letters(vector<string>& word_list) {
-    string letters="";
-    for (auto word:word_list) {
-        for(auto c:word) {
-            if(letters.find(c)==string::npos) {
-                letters+=c;
+int number_of_letters(vector<string>& word_list)
+{
+    string letters = "";
+    for (auto word : word_list) {
+        for (auto c : word) {
+            if (letters.find(c) == string::npos) {
+                letters += c;
             }
         }
     }
@@ -87,13 +92,14 @@ int number_of_letters(vector<string>& word_list) {
 
 // returns the pair with the vector with moste uniq letters
 //template<typename T>
-pair<string, vector<string> > get_most_unique_letters(map<string, vector<string> >& family_map) {
+pair<string, vector<string> > get_most_unique_letters(map<string, vector<string> >& family_map)
+{
     pair <string, vector<string> > ret_map_pair;
     int max_len(0), tmp(0);
 
-    for(auto pair_in_map:family_map) {
+    for (auto pair_in_map : family_map) {
         tmp = number_of_letters(pair_in_map.second);
-        if(tmp > max_len) {
+        if (tmp > max_len) {
             max_len = tmp;
             ret_map_pair = pair_in_map;
         }
@@ -102,22 +108,24 @@ pair<string, vector<string> > get_most_unique_letters(map<string, vector<string>
 }
 
 // returns the number of dashes in word
-int count_dash(string word) {
+int count_dash(string word)
+{
     int ret(0);
     for (char c : word) {
-        if(c == '-') ret++;
+        if (c == '-') ret++;
     }
     return ret;
 }
 
 // returns the pair with moste dashes in the key string
 //template<typename T>
-pair<string, vector<string> > get_most_dashes_word_list(map<string, vector<string> >& family_map) {
+pair<string, vector<string> > get_most_dashes_word_list(map<string, vector<string> >& family_map)
+{
     pair <string, vector<string> > ret_map_pair;
     int max_dash(0), tmp_dash(0);
-    for(auto pair_in_map : family_map) {
+    for (auto pair_in_map : family_map) {
         tmp_dash = count_dash(pair_in_map.first);
-        if(tmp_dash > max_dash) {
+        if (tmp_dash > max_dash) {
             max_dash = tmp_dash;
             ret_map_pair = pair_in_map;
         }
@@ -127,31 +135,34 @@ pair<string, vector<string> > get_most_dashes_word_list(map<string, vector<strin
 
 // loop to get the user to enter valid number of letters
 template<typename T>
-int get_valid_length(T& myMap) {
+int get_valid_length(T& myMap)
+{
     int input;
     do {
 
         cout << "number of letters : ";
         cin >> input;
 
-    } while(myMap.find(input) == myMap.end());
+    } while (myMap.find(input) == myMap.end());
     return input;
 }
 
 //loop to get nummber of guesses
-int get_guesses() {
+int get_guesses()
+{
     int input;
     do {
         cout << "number of guesses min is 1 and max is 26 : ";
         cin >> input;
 
-    } while(input <= 0 || input > 26);
+    } while (input <= 0 || input > 26);
 
     return input;
 }
 
 // return true if the user wonts to see the word list
-bool show_list() {
+bool show_list()
+{
     char input;
     cout << "do you wont to see the word list ? type y or n : ";
     cin >> input;
@@ -160,8 +171,9 @@ bool show_list() {
 
 // print a word list
 template<typename T>
-void print_word_list(T& word_list) {
-    for (auto word:word_list) {
+void print_word_list(T& word_list)
+{
+    for (auto word : word_list) {
         cout << word << " ";
     }
     cout << endl;
@@ -169,22 +181,24 @@ void print_word_list(T& word_list) {
 }
 
 // returns the new! letter enterd by the user
-char get_letter(string guesst) {
+char get_letter(string guesst)
+{
     char ret;
     do {
         cout << "guess a letter :";
         cin >> ret;
         ret = tolower(ret);
-    } while(alphabet.find(ret) == string::npos || guesst.find(ret) != string::npos);
+    } while (alphabet.find(ret) == string::npos || guesst.find(ret) != string::npos);
     return ret;
 }
 
 // start the game
-void play_game(int len, int gue, bool show, vector<string> word_list) {
+void play_game(int len, int gue, bool show, vector<string> word_list)
+{
     string letters_guesst = "", word_to_guess(len, '-');
     char letter;
-    while(word_to_guess.find('-') != string::npos && gue > 0) {
-        cout << "you have " << gue <<" guesses left" << endl;
+    while (word_to_guess.find('-') != string::npos && gue > 0) {
+        cout << "you have " << gue << " guesses left" << endl;
         cout << "letters you have guesst : " << letters_guesst << endl;
         cout << "the word to guess locks like this : " << word_to_guess << endl;
         if (show) print_word_list(word_list);
@@ -193,20 +207,20 @@ void play_game(int len, int gue, bool show, vector<string> word_list) {
 
         map<string, vector<string> > family = getfamily(word_list, letter, word_to_guess);
         pair<string, vector<string> > word_list_pair;
-        if (gue>10) {
+        if (gue > 10) {
             word_list_pair = get_biggest_word_list(family);
         } else {
             word_list_pair = get_most_unique_letters(family);
         }
 
-        if(word_to_guess == word_list_pair.first) {
+        if (word_to_guess == word_list_pair.first) {
             gue--;
         }
         word_to_guess = word_list_pair.first;
         word_list = word_list_pair.second;
     }
 
-    if(word_to_guess.find("-") == string::npos) {
+    if (word_to_guess.find("-") == string::npos) {
         cout << "nice you won!!! " << word_to_guess << endl;
     } else {
         cout << "you lost!!! the word was : " << word_list[0] << endl;
@@ -216,7 +230,8 @@ void play_game(int len, int gue, bool show, vector<string> word_list) {
 
 // the loop to restart the game after played
 template<typename T>
-void play_game_loop(T& myMap) {
+void play_game_loop(T& myMap)
+{
     char input;
     int len, gue;
     bool show;
@@ -229,12 +244,13 @@ void play_game_loop(T& myMap) {
         cout << "to quit type q : ";
         cin >> input;
 
-    } while(input != 'q');
+    } while (input != 'q');
     return;
 }
 
 // main to start the program
-int main() {
+int main()
+{
     map<int, vector<string> > myMap;
     fillmap(myMap);
 
--- lab2/evilhangman/src/tmp/evilhangman_vad.cpp
+++ lab2/evilhangman/src/tmp/evilhangman_vad.cpp
@@ -13,15 +13,16 @@ const string alphabet  = "abcdefghijklmnopqrstuvwxyz";
 
 // fillmap files the map with nummber of letters and a vector with all words with that length
 template<typename T>
-void fillmap(map<int, T>& myMap){
+void fillmap(map<int, T>& myMap)
+{
     string word;
     ifstream myReader;
     myReader.open("dictionary.txt");
-    while(myReader >> word){
-        if (myMap.find(word.length()) != myMap.end()){
-                myMap.find(word.length())->second.push_back(word);
-        }else{
-            myMap.insert(pair<int, T>(word.length(), T{word}));
+    while (myReader >> word) {
+        if (myMap.find(word.length()) != myMap.end()) {
+            myMap.find(word.length())->second.push_back(word);
+        } else {
+            myMap.insert(pair<int, T>(word.length(), T {word}));
         }
     }
     myReader.close();
@@ -29,24 +30,26 @@ void fillmap(map<int, T>& myMap){
 
 // get_family_string return the family string of that word
 // baste on the word start
-string get_family_string(string word, char c, string start){
-   for (size_t i = 0; i < word.length(); i++){
-       if (start[i] == '-' && word[i] == c){
-           start[i] = c;
-       }
-   }
-   return start;
+string get_family_string(string word, char c, string start)
+{
+    for (size_t i = 0; i < word.length(); i++) {
+        if (start[i] == '-' && word[i] == c) {
+            start[i] = c;
+        }
+    }
+    return start;
 }
 
 // getfamily returns a map with familly string as key and a vector of string as word list
 template<typename T>
-map<string, T> getfamily(T& word_list, char c, string start){
+map<string, T> getfamily(T& word_list, char c, string start)
+{
     map<string, T> ret;
-    for(auto word:word_list){
+    for (auto word : word_list) {
         string family_string = get_family_string(word, c, start);
-        if(ret.find(family_string) == ret.end()){
-            ret.insert(ret.begin(), pair<string, T>(family_string, T{word}));
-        }else{
+        if (ret.find(family_string) == ret.end()) {
+            ret.insert(ret.begin(), pair<string, T>(family_string, T {word}));
+        } else {
             ret.find(family_string)->second.push_back(word);
         }
     }
@@ -56,13 +59,14 @@ map<string, T> getfamily(T& word_list, char c, string start){
 // returns a pair with family string as key and a vector as element
 // the pair returnd is the one with bigest vector
 //template<typename T>
-pair<string, vector<string> > get_bigest_word_list(map<string, vector<string> >& family_map){
+pair<string, vector<string> > get_bigest_word_list(map<string, vector<string> >& family_map)
+{
     pair <string, vector<string> > ret_map_pair;
     int max_len(0), tmp;
 
-    for(auto pair_in_map:family_map){
+    for (auto pair_in_map : family_map) {
         tmp = pair_in_map.second.size();
-        if(tmp > max_len){
+        if (tmp > max_len) {
             max_len = tmp;
             ret_map_pair = pair_in_map;
         }
@@ -72,31 +76,34 @@ pair<string, vector<string> > get_bigest_word_list(map<string, vector<string> >&
 
 // loop to get the user to enter valid number of letters
 template<typename T>
-int get_valid_length(T& myMap){
+int get_valid_length(T& myMap)
+{
     int input;
-    do{
+    do {
 
         cout << "number of letters : ";
         cin >> input;
 
-    }while(myMap.find(input) == myMap.end());
+    } while (myMap.find(input) == myMap.end());
     return input;
 }
 
 //loop to get nummber of guesses
-int get_guesses(){
+int get_guesses()
+{
     int input;
-    do{
+    do {
         cout << "number of guesses min is 1 and max is 26 : ";
         cin >> input;
 
-    }while(input <= 0 || input > 26);
+    } while (input <= 0 || input > 26);
 
     return input;
 }
 
 // return true if the user wonts to see the word list
-bool show_list(){
+bool show_list()
+{
     char input;
     cout << "do you wont to see the word list ? type y or n : ";
     cin >> input;
@@ -105,8 +112,9 @@ bool show_list(){
 
 // print a word list
 template<typename T>
-void print_word_list(T& word_list){
-    for (auto word:word_list){
+void print_word_list(T& word_list)
+{
+    for (auto word : word_list) {
         cout << word << " ";
     }
     cout << endl;
@@ -114,22 +122,24 @@ void print_word_list(T& word_list){
 }
 
 // returns the new! letter enterd by the user
-char get_letter(string guesst){
+char get_letter(string guesst)
+{
     char ret;
-    do{
+    do {
         cout << "guess a letter :";
         cin >> ret;
         ret = tolower(ret);
-    }while(alphabet.find(ret) == string::npos || guesst.find(ret) != string::npos);
+    } while (alphabet.find(ret) == string::npos || guesst.find(ret) != string::npos);
     return ret;
 }
 
 // start the game
-void play_game(int len, int gue, bool show, vector<string> word_list){
+void play_game(int len, int gue, bool show, vector<string> word_list)
+{
     string letters_guesst = "", word_to_guess(len, '-');
     char letter;
-    while(word_to_guess.find('-') != string::npos && gue > 0){
-        cout << "you have " << gue <<" guesses left" << endl;
+    while (word_to_guess.find('-') != string::npos && gue > 0) {
+        cout << "you have " << gue << " guesses left" << endl;
         cout << "letters you have guesst : " << letters_guesst << endl;
         cout << "the word to guess locks like this : " << word_to_guess << endl;
         if (show) print_word_list(word_list);
@@ -138,7 +148,7 @@ void play_game(int len, int gue, bool show, vector<string> word_list){
 
         map<string, vector<string> > family = getfamily(word_list, letter, word_to_guess);
         pair<string, vector<string> > word_list_pair = get_bigest_word_list(family);
-        if(word_to_guess == word_list_pair.first){
+        if (word_to_guess == word_list_pair.first) {
             gue--;
         }
 
@@ -146,9 +156,9 @@ void play_game(int len, int gue, bool show, vector<string> word_list){
         word_list = word_list_pair.second;
     }
 
-    if(word_to_guess.find("-") == string::npos){
+    if (word_to_guess.find("-") == string::npos) {
         cout << "nice you won!!! " << word_to_guess << endl;
-    }else{
+    } else {
         cout << "you lost!!! the word was : " << word_list[0] << endl;
     }
     return;
@@ -156,11 +166,12 @@ void play_game(int len, int gue, bool show, vector<string> word_list){
 
 // the loop to restart the game after played
 template<typename T>
-void play_game_loop(T& myMap){
+void play_game_loop(T& myMap)
+{
     char input;
     int len, gue;
     bool show;
-    do{
+    do {
         len = get_valid_length(myMap);
         gue = get_guesses();
         show = show_list();
@@ -169,12 +180,13 @@ void play_game_loop(T& myMap){
         cout << "to quit type q : ";
         cin >> input;
 
-    }while(input != 'q');
+    } while (input != 'q');
     return;
 }
 
 // main to start the program
-int main() {
+int main()
+{
     map<int, vector<string> > myMap;
     fillmap(myMap);
 
